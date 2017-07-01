7ac84caf76c376c8c5b3b82a69b31e0e3a731e89
Hanwen Cheng <heawen.cheng@gmail.com>
2017-07-01 13:14:54 +0200

    :bathtub: test version 1, for medium sample size 100

--- SelectionDefinition/spec/correctness.spec.js
+++ SelectionDefinition/spec/correctness.spec.js
@@ -1,6 +1,37 @@
-describe('haha', ()=>{
-    it('works with jasmine', ()=>{
-        expect(true).toBe(true)
-    })
-})
+const {maps, positions} = require('../src/genTestSets');
+const algorithm = require('../src/main');
+const _ = require('lodash');
+
+const getCenter = p => ({x: p.x + p.width/2, y: p.y + p.height/2});
+const distance = (a, b) => {
+  const c1 = getCenter(a);
+  const c2 = getCenter(b);
+  return Math.pow(c1.x - c2.x, 2) + Math.pow (c1.y - c2.y ,2);
+};
+const testSprite = (sprite, position, minDistance, selected) => {
+  const testDistance = distance(sprite, position);
+  if(testDistance === minDistance){
+    return !(sprite.x < selected.x || sprite.y < selected.y)
+  } else if (testDistance > minDistance){
+    return true
+  }
+  return false
+};
+
+describe('haha', () => {
+  it('works with jasmine', () => {
+    expect(true).toBe(true)
+  });
+
+  it('should find the correct sprite', () => {
+    const correctness= _.every(positions, (position)=> {
+      _.every(maps, (map)=>{
+        let selectedSprite = algorithm(map),
+          minDistance = distance(selectedSprite, position);
+        return _.every(map, _.curryRight(testSprite)(selectedSprite)(minDistance)(position))
+      });
+    });
+    expect(correctness).toBe(true);
+  })
+});
 
---
+++ SelectionDefinition/src/genTestSets.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const
+  mapsNumber = 5,
+  positionsNumber = 3,
+  sampleSize = 100,
+  maxWidth = 1024,
+  maxHeight = 1024;
+
+//use integer value for applying A/B select
+const generateSprite = () => {
+  const x = Math.floor(Math.random() * (maxWidth + 1)),
+    y = Math.floor(Math.random() * (maxHeight + 1)),
+    width = Math.floor(Math.random() * (maxWidth - x + 1)),
+    height = Math.floor(Math.random() * (maxHeight - y + 1));
+  return { x, y, width, height }
+};
+
+const generatePosition = () => {
+  const x = Math.random() * maxWidth,
+    y = Math.random() * maxHeight;
+  return { x, y }
+};
+
+
+const mapArray = (arraySize, mapFunction) => _
+  .chain(new Array(arraySize))
+  .map(mapFunction)
+  .value();
+
+const maps = mapArray(mapsNumber, ()=> mapArray(sampleSize, generateSprite));
+const positions = mapArray(positionsNumber, ()=> generatePosition());
+
+module.exports = {
+  maps, positions
+};
+
+
+// fs.writeFile("./tmp/map", "Hey there!", function(err) {
+//     if(err) {
+//         return console.log(err);
+//     }
+//
+//     console.log("The file was saved!");
+// });
